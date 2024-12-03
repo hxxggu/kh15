dac8b1b87c3b7407cd9215051b28e2f63e09ace1
hxxggu <sita06280@gmail.com>
2024-12-03 13:03:31 +0900

    정수와 변수를 사용한 예제

--- day02/src/data/Test01데이터.java
+++ day02/src/data/Test01데이터.java
@@ -9,9 +9,10 @@ public class Test01데이터 {
 		//: 프로그래밍에서는 단위가 없고 값만 존재
 		//: 값을 어떻게 구분할 것인가가 문제
 		// 1. 숫자 : 일반적인 계산을 위한 값 (소수점 유무로 구분)
+		// 		-정수(소수점X) : byte, short, int(기본값), long
 		// 2. 기타 : 일반적인 계산을 할 수 없는 값
 		
-		int a=10;
-		System.out.println(a);
+		int a=10; //a라는 int 크기의 컵을 준비해서 10을 담아두겠다
+		System.out.println(a); //a컵에 담긴 내용을 화면에 출력하라
 	}
 }
