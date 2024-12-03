0c0ac002b764eb447295248ced663c7266260a49
hxxggu <sita06280@gmail.com>
2024-12-03 13:18:52 +0900

    예제 코드 작성

---
+++ day02/src/data/Test예제.java
@@ -0,0 +1,12 @@
+package data;
+
+public class Test예제 {
+
+	public static void main(String[] args) {
+		int audult = 15000;
+		int child = 10000;
+		int result = (audult*5)+(child*3);
+		System.out.println("총 금액은 "+ result + " 입니다.");
+	}
+	
+}
