add134d75f7329712de7caf083432c6d3b395899
hxxggu <sita06280@gmail.com>
2024-12-03 15:51:01 +0900

    예제 수정

--- day02/src/data/Test07주차요금계산기.java
+++ day02/src/data/Test07주차요금계산기.java
@@ -8,17 +8,20 @@ public class Test07주차요금계산기 {
 		//입력
 		int h1 = 12, m1 = 40;
 		int h2 = 16, m2 = 15;
-		int price = 3000;
+		int period = 30; //단위기간
+		int price = 3000; //단위요금
 		
 		//처리
 		int t1 = h1*60+m1; //분으로 변환
 		int t2 = h2*60+m2; //분으로 변환
-		int t3 = t2-t1;
+		int t3 = t2-t1; //총 주차시간(분)
 		
 		int h3 = t3/60;
 		int m3 = t3%60;
 		
-		int total = price*h3; //오늘만 사는 코드 (확장성 떨어지는 코드/세부조정 불가능)
+		int total = t3/period*price;
+		//total = t3에 단위기간이 몇 번 포함되는지*price
+		//total = price*h3 >오늘만 사는 코드(확장성 떨어지는 코드/세부조정 불가능)
 		
 		//출력
 		System.out.println("주차시간 : "+h3+"시간"+m3+"분");
