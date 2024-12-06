41e2d3ad8cccada4e52b9869458e82a4a9c877e6
hxxggu <sita06280@gmail.com>
2024-12-06 13:13:09 +0900

    반복문

---
+++ day5/src/loop/Test01반복문.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class Test01반복문 {
+	public static void main(String[] args) {
+		//반복
+		//: 동일하거나 규칙이 있는 비슷한 내용들을 계속 실행하는 것
+		//for, while, do-while 구문이 존재
+		
+		//Ex. hello 를 화면에 10번 출력하시오
+		for(int i=1 ; i<=3 ; i++) { //10번
+			System.out.println("hello");
+			System.out.println("i = "+i);
+		}
+	}
+}
