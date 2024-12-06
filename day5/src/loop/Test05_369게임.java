6dc055d578eab82df2fb58b1e3111149432b106c
hxxggu <sita06280@gmail.com>
2024-12-06 15:32:31 +0900

    369게임(본인 코드 / 강사님 피드백)

--- day5/src/loop/Test05_369게임.java
+++ day5/src/loop/Test05_369게임.java
@@ -17,8 +17,14 @@ public class Test05_369게임 {
 			}
 		}
 		
+		int n = 13;
+		System.out.println(n/10%3 == 0); //십의 자리가 3인 경우 출력하라
+		System.out.println(n%10%3 == 0); //일의 자리가 3인 경우 출력하라
+		
+		
 		for(int i=1; i<100; i++) {
-			if((i%10==i/10)&&(i%3==0)) {
+//			if((i%10==i/10)&&(i%3==0)) {
+			if((i/10%3==0) && (i%10%3==0)) {
 				System.out.println("짝짝");
 			} else if(i/10==3 || i/10==6 || i/10==9) {
 				System.out.println("짝");
