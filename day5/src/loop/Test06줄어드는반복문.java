0b581df3a2ece34b393e0951940a9ea202e7c5d8
hxxggu <sita06280@gmail.com>
2024-12-06 15:18:45 +0900

    줄어드는 반복문

---
+++ day5/src/loop/Test06줄어드는반복문.java
@@ -0,0 +1,13 @@
+package loop;
+
+public class Test06줄어드는반복문 {
+
+	public static void main(String[] args) {
+		
+		//Ex. 10부터 1까지 줄어드는 카운트 다운 구현
+		
+		for(int i=10; i>=1; i--) {
+			System.out.println(i);
+		} System.out.println("땡");
+	}
+}
