47dca96d010a9c1dfd92da1d14bc7031448247b1
정정희 <@DESKTOP-4936DRC>
2024-12-07 16:36:56 +0900

    심화 구구단 게임 피드백 전

--- day5/src/loop2/Test05구구단게임.java
+++ day5/src/loop2/Test05구구단게임.java
@@ -9,22 +9,33 @@ public class Test05구구단게임 {
 		Scanner sc = new Scanner(System.in);
 		Random r = new Random();
 
-		
-		int correct = 0, wrong =0;
-		
-		for(int i=0; i<10; i++) {
+		int correct = 0, wrong = 0;
+		int easyScore = 10, hardScore = 20;
+		int totalScore = 0, combo  = 0;
+ 		
+		for(int i=1; i<=10; i++) {
 			int num1 = r.nextInt(8)+2;
 			int	num2 = r.nextInt(9)+1;
 			
 			System.out.print(num1+"X"+num2+"=");
-			int answer = sc.nextInt();
-			if(answer == num1*num2) {
+			int inputNum = sc.nextInt();
+			int answer = num1*num2;
+			
+			if(inputNum == answer) {
+				combo++;
+				if(num1>6) {
+					totalScore += easyScore*combo;
+				} else {
+					totalScore += hardScore*combo;
+				}
 				correct++;
 			} else {
 				wrong++;
+				combo = 0;
 			}
 		}
 		
-		System.out.println("정답 : "+correct+"개, 오답 : "+wrong+"개");
+		System.out.println("정답 : "+correct+"개\n오답 : "+wrong+"개");
+		System.out.println("총점 : "+totalScore+"점");
 	}
 }
