0a805064b8699f7b738ed2ac5151877a694629a3
hxxggu <sita06280@gmail.com>
2024-12-09 12:46:18 +0900

    백준 예제 풀이

--- baekjoon_practice/src/loop/Main2739.java
+++ baekjoon_practice/src/loop/Main2739.java
@@ -3,12 +3,12 @@ package loop;
 import java.util.Scanner;
 
 public class Main2739 {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		for(int i=1; i<=9;i++) {
-			System.out.println(N+"*"+i+"="+(N*i));
+			int result = N*i;
+			System.out.println(N+" * "+i+" = "+result);
 		}
 	}
 }
