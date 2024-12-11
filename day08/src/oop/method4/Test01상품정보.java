462752861f77a0985697267b71801caba97809d7
hxxggu <sita06280@gmail.com>
2024-12-11 16:50:03 +0900

    수정 보류

--- day08/src/oop/method4/Test01상품정보.java
+++ day08/src/oop/method4/Test01상품정보.java
@@ -8,10 +8,10 @@ public class Test01상품정보 {
 		Product p3 = new Product();
 		Product p4 = new Product();
 		
-		p1.init("비김면", "라면", 16800, "없음", 2, true);
-		p2.init("크림대빵", "제과", 6500, "없음", 2, false);
-		p3.init("점보도시락", "라면", 8500, "", 3, true);
-		p4.init("공간춘", "라면", 12300, "", 3, false);
+		//p1.init("비김면", "라면", 16800, "없음", 2, true);
+		//p2.init("크림대빵", "제과", 6500, "없음", 2, false);
+		//p3.init("점보도시락", "라면", 8500, "", 3, true);
+		//p4.init("공간춘", "라면", 12300, "", 3, false);
 		
 		p1.show();
 		p2.show();
