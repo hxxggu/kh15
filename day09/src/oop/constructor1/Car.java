b84146db2fa9be72028f5a4145d5b2204f96c956
hxxggu <sita06280@gmail.com>
2024-12-12 11:12:41 +0900

    Test01생성자

--- day09/src/oop/constructor1/Car.java
+++ day09/src/oop/constructor1/Car.java
@@ -32,14 +32,14 @@ public class Car {
 		this.speed = speed;
 	}
 	
-	//초기화 메서드
-	public void init(String name, String color, int price, int speed) {
-		this.setName(name);
-		this.setColor(color);
-		this.setPrice(price);
-		this.setSpeed(speed);
-	}
-	
+//초기화 메서드
+//	public void init(String name, String color, int price, int speed) {
+//		this.setName(name);
+//		this.setColor(color);
+//		this.setPrice(price);
+//		this.setSpeed(speed);
+//	}
+//	
 	//출력
 	public void show() {
 		System.out.println("[ 차량 정보 ] ");
@@ -48,5 +48,27 @@ public class Car {
 		System.out.println("가격 : " + this.getPrice());
 		System.out.println("최대 속도 : " + this.getSpeed());
 	}
+	
+	//생성자(constructor)
+	//: 인스턴스를 생성할 때 정보를 설정할 수 있는 용도의 구문
+	//: 없어도 클래스는 인스턴스 생성을 위한 도구이므로 자동으로 한 개를 생성
+	//public car(){}  //기본 생성자(default constructor)
+	
+	//: init 메서드와 하는 역할이 같지만 "생성하면서" 설정한다는 것이 다름
+	//: 생성자를 만들면 해당하는 생성자로 생성 방법이 고정됨(강제됨)
+	//: 생성자도 오버로딩이 가능
+	//: 생성자는 반드시 클래스와 이름이 동일해야 함
+	public Car(String name, String color, int price, int speed) {
+		this.setName(name);
+		this.setColor(color);
+		this.setPrice(price);
+		this.setSpeed(speed);
+	}
+	
+	
+	
+	
+	
+	
 	
 }
--- day09/src/oop/constructor1/Test01생성자.java
+++ day09/src/oop/constructor1/Test01생성자.java
@@ -4,12 +4,11 @@ public class Test01생성자 {
 
 	public static void main(String[] args) {
 		
-		Car a = new Car();
+//		Car a = new Car();
+//		a.init("소나타", "블랙", 20000000, 200);
+//		a.show();
 		
-		a.init("소나타", "블랙", 20000000, 200);
-		
-		a.show();
-
+		Car a = new Car("소나타", "블랙", 20000000, 200);
 	}
 
 }
