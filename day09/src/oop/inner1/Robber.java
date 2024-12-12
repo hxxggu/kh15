b4afd26a7ec10d2ea8d626f5bd305fc05056e2db
hxxggu <sita06280@gmail.com>
2024-12-12 16:46:08 +0900

    중첩 클래스

---
+++ day09/src/oop/inner1/Robber.java
@@ -0,0 +1,14 @@
+package oop.inner1;
+
+//도둑도 총을 가질 수 있나요? 안되야 하는데
+public class Robber {
+//	private Gun gun;
+//	
+//	public void setGun(Gun gun) {
+//		this.gun = gun; 
+//	}
+//	public Gun getGun() {
+//		return gun;
+//	}
+
+}
