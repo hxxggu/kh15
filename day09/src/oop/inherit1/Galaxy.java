6f6c927967e51ace9fcd97888b2f97054b064126
hxxggu <sita06280@gmail.com>
2024-12-12 17:06:51 +0900

    상속

---
+++ day09/src/oop/inherit1/Galaxy.java
@@ -0,0 +1,6 @@
+package oop.inherit1;
+
+//휴대폰을 상속받아 확장(extend)하여 구현
+public class Galaxy extends Phone {
+	public void samsungPay() {}
+}
---
+++ day09/src/oop/inherit1/Phone.java
@@ -0,0 +1,13 @@
+package oop.inherit1;
+
+//휴대폰의 공통 내용
+public class Phone {
+	private String number;
+	private String color;
+	//setter,getter 생략
+	public void call() {}
+	public void sms() {}
+	public void camera() {}
+	public void internet() {}
+	public void game() {}
+}
