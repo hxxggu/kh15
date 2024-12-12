73d13ee160f486d46d027b958861eef5658dcb21
hxxggu <sita06280@gmail.com>
2024-12-12 09:31:54 +0900

    과제

--- day08/src/oop/method6/Phone.java
+++ day08/src/oop/method6/Phone.java
@@ -6,76 +6,72 @@ public class Phone {
 	int price;
 	int term;
 	
-	void init(String name, String company, int price) { //약정개월 X
-		this.setName = name;
-		this.company = company;
-		this.price = price;
+	void init(String name, String company, int price) {
+		this.init(name, company, price, 0);
 	}
 	
-	void init(String name, String company, int price, int term) { //약정개월 O
-		this.name = name;
+	void init(String name, String company, int price, int term) {
+		this.setName(name);
 		this.setCompany(company);
 		this.setPrice(price);
 		this.setTerm(term);
 	}
+	
 	void setName(String name) {
 		this.name = name;
 	}
 	
 	void setCompany(String company) {
-		if(company!="SK") {
-			return;
-		} else if(company!="KT") {
-			return;
-		} else if(company!="LG") {
-			return;
-		} else if(company!="알뜰폰") {
+		if(company!="SK" && company!="KT" && company!="LG" && company!="알뜰폰") {
 			return;
 		}
+		this.company = company;
 	}
 	
 	void setPrice(int price) {
-		if(0>price) {
+		if(0 > price) {
 			return;
 		}
 		this.price = price;
 	}
 	
 	void setTerm(int term) {
-		if(term != 0) {
-			return;
-		} else if(term != 24) {
-			return;
-		} else if(term != 30) {
-			return;
-		} else if(term != 36) {
+		if(term!=0 && term!=24 && term!=30 && term!=36) {
 			return;
 		}
+		this.term = term;
 	}
+	
+	String getName() {
+		return this.name;
+	}
+	
+	String getCompany() {
+		return this.company;
 	
 	int getPrice() {
 		return this.price;
 	}
 	
-	int getMonth() {
-		
+	int getTerm() {
+		return this.term;
 	}
 	
-	
-	
+	float getPay() {
+		return (float)this.getPrice()/this.getTerm();
+	}
+		
 	void show() {
-		if(term == true) {
-			System.out.println("이름 : "+this.name);
-			System.out.println("통신사 : "+this.company);
-			System.out.println("가격 : "+this.price);
-			System.out.println("약정 개월 : "+this.term);
-			System.out.println("월 분납금 : ");
-		} else {
-			System.out.println("이름 : "+this.name);
-			System.out.println("통신사 : "+this.company);
-			System.out.println("가격 : "+this.price);
-			System.out.println("약정 개월 : 미설정");
+		System.out.println("[ 기기 정보 ]");
+		System.out.println("이름 : "+this.getName());
+		System.out.println("통신사 : "+this.getCompany());
+		System.out.println("가격 : "+this.getPrice()+"원");
+		if(this.getTerm() != 0) {
+			System.out.println("약정 개월 : "+this.getTerm()+"개월");
+			System.out.println("월 분납금 : "+this.getPay()+"원");
+			} else {
+				System.out.println("약정 개월 : 미설정");
+			}
 		}
 	}
-	
 }
--- day08/src/oop/method6/Test01핸드폰.java
+++ day08/src/oop/method6/Test01핸드폰.java
@@ -8,8 +8,8 @@ public class Test01핸드폰 {
 		Phone d = new Phone();
 		
 		a.init("갤럭시Fold6", "SK", 2200000);
-		b.init("갤럭시Fold6", "KT", 2150000, true);
-		c.init("아이폰16", "LG", 2100000, true);
+		b.init("갤럭시Fold6", "KT", 2150000, 24);
+		c.init("아이폰16", "LG", 2100000, 36);
 		d.init("아이폰16", "SK", 2150000);
 		
 		a.show();
