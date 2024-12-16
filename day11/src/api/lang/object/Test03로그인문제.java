f5434d03537e10cb3a41f8677992ef62167fe296
hxxggu <sita06280@gmail.com>
2024-12-16 13:52:16 +0900

    로그인 문제

---
+++ day11/src/api/lang/object/Test03로그인문제.java
@@ -0,0 +1,21 @@
+package api.lang.object;
+
+import java.util.Scanner;
+
+public class Test03로그인문제 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("ID : ");
+		String inputId = sc.next();
+		System.out.print("Password : ");
+		String inputPw = sc.next();
+		
+		String baseId = new String("khacademy");
+		String basePw = new String("123456");
+		
+		if(inputId.equalsIgnoreCase(baseId) && inputPw.equalsIgnoreCase(basePw)) {
+			System.out.println("로그인 성공");
+		} else System.out.println("로그인 실패");
+	}
+}
---
+++ day11/src/api/lang/object/Test03로그인문제00.java
@@ -0,0 +1,30 @@
+package api.lang.object;
+
+import java.util.Scanner;
+
+public class Test03로그인문제00 {
+
+	public static void main(String[] args) {
+		
+		
+		//입력
+		Scanner sc = new Scanner(System.in);
+		System.out.print("아이디 : ");
+		String id = sc.next();
+//		String id = "khacademy";
+		System.out.print("비밀번호 : ");
+		String password = sc.next();
+//		String password = "123456";
+		
+//		boolean valid = 아이디 일치 && 비밀번호 일치;
+//		boolean valid = id == "khacademy" && password == "123456";
+		boolean valid = id.equalsIgnoreCase("khacademy") && password.equalsIgnoreCase("123456");
+		if(valid) {
+			System.out.println("로그인 성공");
+		} else {
+			System.out.println("로그인 실패");
+		}
+
+	}
+
+}
