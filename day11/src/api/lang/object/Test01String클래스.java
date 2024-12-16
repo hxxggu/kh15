53bb899ba0d7cf8d9cc62f4c87b1c5e66751dad5
hxxggu <sita06280@gmail.com>
2024-12-16 13:16:16 +0900

    문자열의 형태

---
+++ day11/src/api/lang/object/Test01String클래스.java
@@ -0,0 +1,32 @@
+package api.lang.object;
+
+public class Test01String클래스 {
+
+	public static void main(String[] args) {
+		//인스턴스 생성
+		//String a = "hello";
+		String a = new String("hello");
+		
+		char[] ch = new char[] {'h','e','l','l','o'};
+		String b = new String(ch);
+		
+		byte[] by = new byte[] {104, 101, 108, 108, 111};
+		String c = new String(by);
+		
+		System.out.println("a = "+a.hashCode());
+		System.out.println("b = "+b.hashCode());
+		System.out.println("c = "+c.hashCode());
+		
+		System.out.println("a = "+a.toString());
+		System.out.println("b = "+b.toString());
+		System.out.println("c = "+c.toString());
+		
+		System.out.println(a == b); //a와 b가 같습니까? (인스턴스를 비교)
+		System.out.println(a.equals(b)); //a와 b가 같습니까? (글자를 비교)
+		
+		String d = "hello"; //문자열의 약식 생성 명령
+		System.out.println(a.equals(d)); //a와 d가 같습니까? (대소문자 구분)
+		System.out.println(a.equalsIgnoreCase(d)); //대소문자 무시하고 비교
+	}
+
+}
---
+++ day11/src/api/lang/object/Test02문자열의형태.java
@@ -0,0 +1,21 @@
+package api.lang.object;
+
+public class Test02문자열의형태 {
+	public static void main(String[] args) {
+		//Q. 그래서 new를 쓰는지 아닌지
+		//: 자바는 new 라는 키워드가 있어야 데이터를 새로 생성
+		//: 자바는 메서드를 실행하기 전에 메모리 소요량을 계산하게 되어 있음
+		
+		String a = "hello"; //미리 정의해둔 문자열에 연결만 하세요
+		String b = "hello"; //미리 정의해둔 문자열에 연결만 하세요
+		String c = new String("hello"); //문자열을 신규 생성하세요
+		String d = new String("hello"); //문자열을 신규 생성하세요
+		
+		System.out.println(a == b); //true
+		System.out.println(a == c); //false
+		System.out.println(a == d); //false
+		
+		System.out.println(a.equals(b)); //true
+		System.out.println(a.equals(c)); //true
+	}
+}
