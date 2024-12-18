35edd92bc63218a1ff465b6d187359fc0bd3bab8
hxxggu <sita06280@gmail.com>
2024-12-18 18:18:50 +0900

    달력 만들기

---
+++ day13/src/api/time/Test05달력만들기.java
@@ -0,0 +1,17 @@
+package api.time;
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class Test05달력만들기 {
+
+	public static void main(String[] args) {
+		Calendar c = Calendar.getInstance();
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("연도 : ");
+		int year = sc.nextInt();
+		System.out.println("월 : ");
+		int month = sc.nextInt();
+	}
+}
