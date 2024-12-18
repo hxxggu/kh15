178b9466cbcc9b0268b63593fda27f37c20d67d6
hxxggu <sita06280@gmail.com>
2024-12-18 18:45:03 +0900

    Test05달력만들기.java 업데이트

--- day13/src/api/time/Test05달력만들기.java
+++ day13/src/api/time/Test05달력만들기.java
@@ -11,7 +11,16 @@ public class Test05달력만들기 {
 		
 		System.out.println("연도 : ");
 		int year = sc.nextInt();
+        c.set(Calendar.YEAR, year);
 		System.out.println("월 : ");
 		int month = sc.nextInt();
+        c.set(Calendar.MONTH, month);
+        Date d = c.getTime();
+        Format f = new SimpleDateFormat("yyyy년 MM월");
+        System.out.println(f.format(d));
+        for(int i=0; i<6; i++) {
+            for(int j=0; j<7; j++) {
+                
+                
 	}
 }
