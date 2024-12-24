bda0864e96dd1634393e4e9238f396a45defb802
hxxggu <sita06280@gmail.com>
2024-12-24 14:03:40 +0900

    바이트 배열 입출력

--- day17/src/api/io/single/Test03파일복사3.java
+++ day17/src/api/io/single/Test03파일복사3.java
@@ -5,7 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class Test03파일복사2 {
+public class Test03파일복사3 {
 	public static void main(String[] args) throws IOException {
 		//Q. sample/origin.txt를 sample/copy.txt로 복사하십시오
 		
@@ -15,8 +15,8 @@ public class Test03파일복사2 {
 		
 		//준비 : 입력용 파일, 입력용 통로, 출력용 파일, 출력용 통로
 		
-		File readTarget = new File("sample/origin.txt");
-		File writeTarget = new File("sample/copy.txt");
+		File readTarget = new File("D:\\kh15\\eclipse\\eclipse.exe");
+		File writeTarget = new File("D:\\kh15\\eclipse\\eclipse_copy.exe");
 		
 		if(!readTarget.exists()) {
 			System.err.println("대상이 존재하지 않습니다");
@@ -28,10 +28,17 @@ public class Test03파일복사2 {
 		FileOutputStream out = new FileOutputStream(writeTarget);
 		//readTarget → in → [프로그램] → out → writeTarget
 		
+		
+		long size = readTarget.length();
+		long cnt = 0L;
+		
 		while(true) {
 			int value = in.read();
 			if(value == -1) break; //EOF
 			out.write(value);
+			cnt++;
+			float percent = cnt*100f/size;
+			System.out.println(cnt + "/" + size);
 		}
 		
 		//통로 정리
---
+++ day17/src/api/io/single/Test05바이트배열입력.java
@@ -0,0 +1,39 @@
+package api.io.single;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Test05바이트배열입력 {
+	public static void main(String[] args) throws IOException {
+		//바이트를 모아서 입력
+		//: 준비물 : 입력용 파일, 입력용 통로, 바이트 배열(카트)
+		
+		File target = new File("sample/singlearray.txt");
+		FileInputStream in = new FileInputStream(target);
+		byte[] buffer = new byte[5]; //buffer : 임시저장소
+		
+		int size;
+		
+		size = in.read(buffer); //읽어들인 데이터를 buffer를 가득 채우도록 입력받으세요  (5개씩)
+		System.out.println("size = " +size);
+		System.out.println(Arrays.toString(buffer)); //arrays : 배열 도우미 / 배열 출력 간소화
+		
+		size = in.read(buffer);
+		System.out.println("size = " +size);
+		System.out.println(Arrays.toString(buffer));
+		size = in.read(buffer);
+		System.out.println("size = " +size);
+		System.out.println(Arrays.toString(buffer));
+		size = in.read(buffer);
+		System.out.println("size = " +size);
+		System.out.println(Arrays.toString(buffer));
+		size = in.read(buffer);
+		System.out.println("size = " +size);
+		System.out.println(Arrays.toString(buffer));
+		size = in.read(buffer);
+		System.out.println("size = " +size);
+		System.out.println(Arrays.toString(buffer));
+	}
+}
