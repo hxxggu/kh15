4b97d8a40a64b404d69e611943b1489a2a776064
hxxggu <sita06280@gmail.com>
2025-01-07 15:11:41 +0900

    프로젝트 이름 수정

--- spring07quiz/src/main/java/com/kh/spring07/Spring07quizApplication.java
+++ spring07quiz/src/main/java/com/kh/spring07/Spring07quizApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring07playerApplication {
+public class Spring07quizApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring07playerApplication.class, args);
+		SpringApplication.run(Spring07quizApplication.class, args);
 	}
 
 }
