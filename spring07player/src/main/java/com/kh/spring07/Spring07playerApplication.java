0ebe83c7f3173873ad7339f964f703b17ebfb06d
hxxggu <sita06280@gmail.com>
2025-01-07 15:09:31 +0900

    spring07player

---
+++ spring07player/src/main/java/com/kh/spring07/Spring07playerApplication.java
@@ -0,0 +1,13 @@
+package com.kh.spring07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring07playerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring07playerApplication.class, args);
+	}
+
+}
