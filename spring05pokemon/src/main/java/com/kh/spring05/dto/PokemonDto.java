ec1ad88705000f256754c73e20fdc6de82094cb9
hxxggu <sita06280@gmail.com>
2025-01-07 15:09:03 +0900

    lombok 추가

--- spring05pokemon/src/main/java/com/kh/spring05/dto/PokemonDto.java
+++ spring05pokemon/src/main/java/com/kh/spring05/dto/PokemonDto.java
@@ -1,42 +1,17 @@
 package com.kh.spring05.dto;
 
+import lombok.Data;
+
 //등록은 왜 하는 걸까?
 //: 인스턴스를 직접 생성하지 않고 스프링에게 생성을 요청하는 것
 //: 특별한 옵션이 없다면 1개만 생성(싱글톤, singleton)
 //: DTO는 여러 개 존재하며 필요할 때마다 만들어야 한다
 //: 등록을 하지 않고 그때그때 new로 만들어서 사용
+//@Setter @Getter @ToString @AllArgsConstructor
+@Data
 public class PokemonDto {
 	private int pokemonNo;
 	private String PokemonName;
 	private String PokemonType;
 	
-	public int getPokemonNo() {
-		return pokemonNo;
-	}
-	public void setPokemonNo(int pokemonNo) {
-		this.pokemonNo = pokemonNo;
-	}
-	public String getPokemonName() {
-		return PokemonName;
-	}
-	public void setPokemonName(String pokemonName) {
-		PokemonName = pokemonName;
-	}
-	public String getPokemonType() {
-		return PokemonType;
-	}
-	public void setPokemonType(String pokemonType) {
-		PokemonType = pokemonType;
-	}
-	
-	public PokemonDto() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	
-	@Override
-	public String toString() {
-		return "PokemonDto [pokemonNo=" + pokemonNo + ", PokemonName=" + PokemonName + ", PokemonType=" + PokemonType
-				+ "]";
-	}
 }
--- spring06country/src/main/java/com/kh/spring06/dto/CountryDto.java
+++ spring06country/src/main/java/com/kh/spring06/dto/CountryDto.java
@@ -1,52 +1,18 @@
 package com.kh.spring06.dto;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+//@Setter @Getter //해당 클래스의 모든 필드값에 setter/getter를 만들어라
+//@NoArgsConstructor //기본생성자
+//@ToString
+@Data
 public class CountryDto {
 	private int countryNo;
 	private String countryName;
 	private String countryCapital;
 	private long countryPopulation;
-	
-	public CountryDto() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public String toString() {
-		return "CountryDto [countryNo=" + countryNo + ", countryName=" + countryName + ", countryCapital="
-				+ countryCapital + ", countryPopulation=" + countryPopulation + "]";
-	}
-
-	public int getCountryNo() {
-		return countryNo;
-	}
-
-	public void setCountryNo(int countryNo) {
-		this.countryNo = countryNo;
-	}
-
-	public String getCountryName() {
-		return countryName;
-	}
-
-	public void setCountryName(String countryName) {
-		this.countryName = countryName;
-	}
-
-	public String getCountryCapital() {
-		return countryCapital;
-	}
-
-	public void setCountryCapital(String countryCapital) {
-		this.countryCapital = countryCapital;
-	}
-
-	public long getCountryPopulation() {
-		return countryPopulation;
-	}
-
-	public void setCountryPopulation(long countryPopulation) {
-		this.countryPopulation = countryPopulation;
-	}
-	
 }
