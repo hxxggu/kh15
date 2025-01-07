18663be298abf0fb6370fea8127f97c2e16ddc04
hxxggu <sita06280@gmail.com>
2025-01-07 16:37:35 +0900

    spring_menu

---
+++ spring07quiz/src/main/java/com/kh/spring07/controller/MenuController.java
@@ -0,0 +1,64 @@
+package com.kh.spring07.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kh.spring07.dao.MenuDao;
+import com.kh.spring07.dto.MenuDto;
+
+
+@RestController
+@RequestMapping("/menu")
+public class MenuController {
+	@Autowired
+	private MenuDao menuDao;
+	
+	@RequestMapping("/add") //등록
+	public String insert(@ModelAttribute MenuDto menuDto) {
+		menuDao.insert(menuDto);
+		return "메뉴 정보 등록 완료";
+	}
+	
+	@RequestMapping("/list") //목록+검색
+	public String list(
+			@RequestParam(required=false) String column,
+			@RequestParam(required=false) String keyword) {
+		boolean search = column!=null && keyword!=null;
+		List<MenuDto> list = search ?
+				menuDao.selectList(column, keyword) : menuDao.selectList();
+		StringBuffer bf = new StringBuffer();
+		if(list.isEmpty()) bf.append("데이터가 존재하지 않습니다");
+		else {
+			bf.append("데이터 개수 : " + list.size() + "<br>");
+			for(MenuDto menuDto : list) {
+				bf.append(menuDto.toString());
+				bf.append("<br>");
+			}
+		} return bf.toString();
+	}
+	
+	@RequestMapping("/detail") //상세
+	public String detail(@RequestParam int menuNo) {
+		MenuDto menuDto = menuDao.selectOne(menuNo);
+		if(menuDto == null) return "존재하지 않는 메뉴 번호";
+		else return menuDto.toString();
+	}
+	
+	@RequestMapping("/edit") //수정
+	public String edit(@ModelAttribute MenuDto menuDto) {
+		boolean success = menuDao.update(menuDto);
+		return success ? "메뉴 정보 수정 완료" : "존재하지 않는 메뉴 번호";
+	}
+	
+	@RequestMapping("/delete") //삭제
+	public String delete(@ModelAttribute MenuDto menuDto) {
+		boolean success = menuDao.update(menuDto);
+		return success ? "메뉴 정보 삭제 완료" : "존재하지 않는 메뉴 번호";
+	}
+	
+}
---
+++ spring07quiz/src/main/java/com/kh/spring07/dao/MenuDao.java
@@ -0,0 +1,83 @@
+package com.kh.spring07.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kh.spring07.dto.MenuDto;
+import com.kh.spring07.mapper.MenuMapper;
+
+@Repository
+public class MenuDao {
+	@Autowired
+	private MenuMapper menuMapper;
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	public void insert(MenuDto menuDto){
+		String sql = "insert into menu("
+				+ "menu_no, menu_name, menu_type,"
+				+ "menu_price, menu_event)"
+				+ "values(menu_seq.nextval, ?, ?, ?, ?)";
+		Object[] data = {
+				menuDto.getMenuName(),
+				menuDto.getMenuType(),
+				menuDto.getMenuPrice(),
+				menuDto.getMenuEvent()
+		};
+		jdbcTemplate.update(sql, data);
+	}
+	
+	public boolean update(MenuDto menuDto) {
+		String sql = "update menu "
+				+"set menu_name = ?, menu_type = ?, "
+				+"menu_price = ?, menu_event = ? "
+				+"where menu_no = ?";
+		Object[] data = {
+				menuDto.getMenuName(),
+				menuDto.getMenuType(),
+				menuDto.getMenuPrice(),
+				menuDto.getMenuEvent(),
+				menuDto.getMenuNo()
+		};
+		int rows = jdbcTemplate.update(sql, data);
+		return rows > 0;
+	}
+	
+	public boolean delete(int menuNo) {
+		String sql = "delete menu where menu_no= ?";
+		Object[] data = {menuNo};
+		return jdbcTemplate.update(sql, data) > 0;
+	}
+	
+	public List<MenuDto> selectList(){
+		String sql = "select * from menu";
+		return jdbcTemplate.query(sql, menuMapper);
+	}
+	
+	private Map<String, String> columnExample = Map.of(
+		"이름", "menu_name",
+		"종류", "menu_type"
+	);
+	
+	public List<MenuDto> selectList(String column, String keyword) {
+		String columnName = columnExample.get(column);
+		if(columnName == null) throw new RuntimeException();
+		String sql = "select * from menu "
+				+ "where instr(" + columnName + ", ?) > 0 "
+				+ "order by " + columnName + " asc, menu_no asc";
+		Object[] data = {keyword};
+		return jdbcTemplate.query(sql, menuMapper, data);
+	}	
+	
+	public MenuDto selectOne(int menuNo) {
+		String sql = "select * from menu where menu_no = ?";
+		Object[] data = {menuNo};
+		List<MenuDto> list = jdbcTemplate.query(sql, menuMapper, data);
+		return list.isEmpty() ? null : list.get(0);
+	}
+	
+}
