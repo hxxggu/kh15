0e0092f2e0f65366d82ffb9e105d0b704f7e8285
hxxggu <hxxggu@gmail.com>
2025-02-24 11:16:04 +0900

    application properties 이메일 저장

---
+++ spring11email/src/main/java/com/kh/spring11/configuration/EmailProperties.java
@@ -0,0 +1,13 @@
+package com.kh.spring11.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+// application.properties 와 같은 설정 파일의 내용을 프로그램에 연동하는 파일
+@Data
+@ConfigurationProperties(prefix = "custom.email")
+public class EmailProperties {
+	private String username; // custom.email.username : custom.email 의 username 을 읽어라
+	private String password; // custom.email.password
+}
