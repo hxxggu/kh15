4b7db7023f31930ef6d8a0d6720c5856678c5de6
hxxggu <hxxggu@gmail.com>
2025-04-01 10:10:43 +0900

    암호화

---
+++ spring12rest/src/test/java/com/kh/spring12/security/Test02XOR암호화.java
@@ -0,0 +1,15 @@
+package com.kh.spring12.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Test02XOR암호화 {
+	
+	@Test
+	public void test() {
+		// 비트 연산자 - &, |, ^, ~
+		// - 비트(bit) 단위로 수행하는 연산
+	}
+
+}
